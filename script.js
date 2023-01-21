556b1371516c62389aa96fb5dbcf7140aa7568e0
Luis Filipe Santos Silva <109242677+Luis-Filipe-Santos@users.noreply.github.com>
2023-01-21 16:39:12 -0300

    f

--- script.js
+++ script.js
@@ -3,28 +3,26 @@ const nlwSetup = new NLWSetup(form);
 const button = document.querySelector("header button");
 
 button.addEventListener("click", add);
+form.addEventListener("change", save);
 
 function add() {
-  const today = "01/01";
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
 
   const dayExists = nlwSetup.dayExists(today);
 
   if (dayExists) {
-    alert("Dia já incluso");
+    alert("Dia já incluso 🔴");
     return;
   }
 
+  alert("Adicionado com sucesso ✅")
   nlwSetup.addDay(today);
 }
 
-// const data = {
-//     run: ['01-23', '01-24', '01-25', '01-26','01-27', '01-28', '01-29', '01-30', '01-31'],
-//     water: ['01-23', '01-24', '01-25', '01-26','01-27', '01-28', '01-29', '01-30', '01-31'],
-//     food: ['01-23', '01-24', '01-25', '01-26','01-27', '01-28', '01-29', '01-30', '01-31'],
-//     bible: ['01-23', '01-24', '01-25', '01-26','01-27', '01-28', '01-29', '01-30', '01-31'],
-//     stude: ['01-23', '01-24', '01-25', '01-26','01-27', '01-28', '01-29', '01-30', '01-31'],
-//     sleep: ['01-23', '01-24', '01-25', '01-26','01-27', '01-28', '01-29', '01-30', '01-31']
-// };
+function save() {
+    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
+}
 
-// nlwSetup.setData(data);
-// nlwSetup.load();
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+nlwSetup.setData(data);
+nlwSetup.load();
