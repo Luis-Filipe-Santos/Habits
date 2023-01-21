af0426b7fd750a24031078cea1da276c446a7065
Luis Filipe Santos Silva <109242677+Luis-Filipe-Santos@users.noreply.github.com>
2023-01-21 14:29:58 -0300

    f

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const form = document.querySelector('#form-habits');
-const nlwSetup = new NLWSetup(form)
-const button = document.querySelector('header button');
+const form = document.querySelector("#form-habits");
+const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-button.addEventListener('click',add);
+button.addEventListener("click", add);
 
 function add() {
-    alert("hello world!");
+  const today = "01/01";
+
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Dia já incluso");
+    return;
+  }
+
+  nlwSetup.addDay(today);
 }
 
 // const data = {
